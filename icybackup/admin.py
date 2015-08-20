6ed88ce3e312de28d23b2a0db0b17f0f4cadd311
Curtis Maloney <curtis@tinbrain.net>
2015-08-20 14:59:45 +1000

    Cleanup with flake8

--- icybackup/admin.py
+++ icybackup/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from .models import GlacierBackup
 
+
 class GlacierBackupAdmin (admin.ModelAdmin):
-	list_display = ('date', 'glacier_id')
-	readonly_fields = ('glacier_id', 'date')
-	
-	def has_add_permission(self, request):
-		return False
-		
-	def has_delete_permission(self, request, obj=None):
-		return False
+    list_display = ('date', 'glacier_id')
+    readonly_fields = ('glacier_id', 'date')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 admin.site.register(GlacierBackup, GlacierBackupAdmin)
